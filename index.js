af2c455b7b93d51ac5c917d8bdc44e74b935c366
Kelvin <kevohmasambu@gmail.com>
2023-12-11 12:02:23 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  if(distance <= 400){
+    return "This one is on me!"
+  }
+  else if(distance > 2000 && distance < 2500){
+    return "I will gladly take your thirty bucks."
+  }
+  else if(distance > 2500){
+    return "No can do."
+  }
+}
+console.log(scuberGreetingForFeet(1300));
+
+
+
+
+function ternaryCheckCity(destination){
+  if(destination === "NYC"){
+    return "Ok, sounds good."
+  }
+  else if(destination !== "NYC"){
+    return "No go."
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+
+function switchOnCharmFromTip(tip){
+  switch(tip){
+  case "generous":
+    return "Thank you so much."
+  case "not as generous":
+    return "Thank you."
+  default:
+    return "Bye." 
+  }
 }
